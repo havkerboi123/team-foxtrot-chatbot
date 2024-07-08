e98d47f7925258dd787ae9e226723619b7405501
havkerboi123 <39674955+havkerboi123@users.noreply.github.com>
2024-07-08 16:10:43 +0500

    Update url_scrapping.py

--- url_scrapping.py
+++ url_scrapping.py
@@ -3,11 +3,11 @@ from langchain_community.document_loaders import AsyncChromiumLoader
 from langchain_community.document_transformers import BeautifulSoupTransformer
 from bs4 import BeautifulSoup
 
-# Function to extract all URLs from a given HTML content
+
 def extract_urls(html_content, base_url):
     soup = BeautifulSoup(html_content, 'html.parser')
     
-    # Find all anchor tags with href attribute
+    
     anchor_tags = soup.find_all('a', href=True)
     
     # Extract and normalize URLs
@@ -15,18 +15,16 @@ def extract_urls(html_content, base_url):
     
     return urls
 
-# Replace 'your_website_url' with the actual URL of the website you want to scrape
 website_url = 'https://teamfoxtrot.pk'
 
-# Load HTML content from the website
+
 loader = AsyncChromiumLoader([website_url])
 html_content = loader.load()
 
-# Use BeautifulSoup to extract URLs
 bs_transformer = BeautifulSoupTransformer()
 docs_transformed = bs_transformer.transform_documents(html_content)
 
-# Extract URLs from the first page (you can loop through all pages if needed)
+# Extractigg  the dasd  uRLs from the first page (you can loop through all pages if needed)
 urls_on_first_page = extract_urls(docs_transformed[0].page_content, website_url)
 
 # Print the extracted URLs
